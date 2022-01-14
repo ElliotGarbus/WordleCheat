3f0b6212d7490d3ceadbe13df7ce46818c4b9b6c
ElliotGarbus <elliotg2@cox.net>
2022-01-14 08:55:52 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,171 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.properties import BooleanProperty
+
+import re
+import json
+
+# TODO: add input_filter to letter input to only take one letter
+# TODO: add input_filter to other inputs to just accept letters, with no repeat letters
+# TODO: make sure 3 lists of letters are mutually exclusive
+# TODO: change search string, not working with just a set of pos unknown letters
+# TODO: Change ScrollView to RecycleView? (or split loading it 2)
+
+kv = """
+<LetterInput>:
+    size_hint: None, None
+    size: 30, 30
+    write_tab: False
+    on_text: app.process_text()
+    
+BoxLayout:
+    orientation: 'vertical'
+    spacing: 5
+    Label:
+        text: 'Wordle Assistant'
+        font_size: 30
+        size_hint_y: None
+        height: 48
+    BoxLayout:
+        size_hint_y: None
+        height: 30
+        Label:  # Placeholder
+        BoxLayout:
+            size_hint_x: None
+            width: 30 * 5
+            id: letters
+        Label:  # Placeholder
+    BoxLayout:
+        size_hint_y: None
+        height: 30
+        Label:
+            size_hint_x: .3
+            text: 'Not in Word'
+        TextInput:
+            id: not_in_word
+            hint_text: 'Enter the letters not in the word'
+            write_tab: False
+            on_text: app.process_text()
+            
+    BoxLayout:
+        size_hint_y: None
+        height: 30
+        Label:
+            size_hint_x: .3
+            text: 'Position Unknown'
+        TextInput:
+            id: pos_unknown
+            hint_text: 'Enter the letters in the word, position unknown'
+            write_tab: False
+            on_text: app.process_text()
+    BoxLayout:
+        size_hint_y: None
+        height: 48
+        Button:
+            text: 'Enter'
+            on_release: app.find_candidates()
+            disabled: app.no_input
+        Button: 
+            text: 'Clear'
+            on_release: app.clear_inputs()
+    ScrollView:
+        GridLayout:
+            cols: 5
+            id: grid
+            row_default_height: 30
+            size_hint_y: None
+            height: self.minimum_height
+"""
+
+
+class LetterInput(TextInput):
+    pass
+
+
+class WordleCheat(App):
+    no_input = BooleanProperty(True)  # used to disable enter
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.words = ''
+
+    def build(self):
+        return Builder.load_string(kv)
+
+    def on_start(self):
+        # create letter inputs
+        for _ in range(5):
+            self.root.ids.letters.add_widget(LetterInput())
+
+        # load words
+        with open('words.json') as f:
+            words = json.load(f)
+        self.words = '\n'.join(words)
+
+    def find_candidates(self):
+        # create the search terms based on what is know, display candidates
+        # get know letters and positions
+        p = self.root.ids
+        known = []
+        for letter in p.letters.children[::-1]:
+            if letter.text.isalpha():
+                known.append(letter.text.lower())
+            else:
+                known.append('.')
+        # letters not in the word
+        if p.not_in_word.text:
+            nots = '[^' + p.not_in_word.text.lower() + '\n]'
+        else:
+            nots = None
+        # print(nots)
+        # letters in word, pos unknown
+        if p.pos_unknown.text:
+            pos_unknown = '[' + p.pos_unknown.text.lower() + ']'
+        else:
+            pos_unknown = None
+        search = []
+        indices = [i for i, v in enumerate(known) if v == '.']
+        if pos_unknown:
+            for index in indices:
+                k = known[::]
+                k[index] = pos_unknown
+                k.insert(0,'(')
+                k.append('\n)|')
+                search.extend(k)
+            search[-1] = '\n)' # remove trailing |
+        else:
+            search = known[::]
+        print(search)
+        search = ''.join(search)
+        if nots:
+            search = search.replace('.', nots)
+        # print(f'{repr(search)}')
+        candidates = re.findall(search, self.words)
+        # print(candidates)
+        if candidates and isinstance(candidates[0], tuple):
+            candidates = [e1.strip() for e in candidates for e1 in e if e1]
+        self.root.ids.grid.clear_widgets()
+        for word in candidates:
+            self.root.ids.grid.add_widget(Label(text=word))
+
+    def clear_inputs(self):
+        p = self.root.ids
+        for letter in p.letters.children:
+            letter.text = ''
+        p.not_in_word.text = ''
+        p.pos_unknown.text = ''
+        self.root.ids.grid.clear_widgets()
+
+    def process_text(self):
+        p = self.root.ids
+        self.no_input = not any([w.text for w in p.letters.children] + [p.not_in_word.text] + [p.pos_unknown.text])
+        # get letters from input
+        letters = [w.text for w in p.letters.children]
+        for letter in letters:
+            if letter in p.pos_unknown.text:
+                p.pos_unknown.text = p.pos_unknown.text.replace(letter, '')
+
+
+WordleCheat().run()
